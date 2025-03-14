824ab6f24e9e0a567994442c6ff3e309b93de84d
Anggunsafitri22 <anggun00iuu@gmail.com>
2025-03-14 08:10:42 +0700

    menambah form

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function showProfile()
+{
+    return view('profile', [
+        'nama' => 'Anggun',
+        'kelas' => 'D3 Manajemen Informatika',
+        'npm' => '2307051004'
+    ]);
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/profile', [ProfileController::class, 'profile']);
+
+Route::get('/profile/{nama}/{kelas}/{npm}', [ProfileController::class, 'profile']);
+
+Route::get('/user/create', [UserController::class, 'create'])->name('user_create');
+
+Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
